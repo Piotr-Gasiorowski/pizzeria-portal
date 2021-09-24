e312806e51beb8849c48faeb2b5cc5992d6f79f2
Piotr Gąsiorowski <p.gasiorowski@wp.pl>
2021-09-24 16:44:04 +0200

    Add views

--- src/components/views/BookedTable/BookedTable.js
+++ src/components/views/BookedTable/BookedTable.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import styles from './BookedTable.module.scss';
+import PropTypes from 'prop-types';
 
-const BookedTable = () => (
+const BookedTable = ({tableId}) => (
   <div className={styles.component}>
     <h2>BookedTable view</h2>
+    {tableId}
   </div>
 );
+
+BookedTable.propTypes = {
+  tableId: PropTypes.string,
+};
 
 export default BookedTable;
